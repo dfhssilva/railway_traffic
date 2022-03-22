1aee932fc43e19375a1e381581e1402ae4e0da2a
David Silva <dfhssilva@protonmail.com>
2022-03-22 19:51:50 +0000

    Add notebook and initial code

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+
+
+def run_query(query):
+    # Select your transport with a defined url endpoint
+    transport = RequestsHTTPTransport(
+        url="https://rata.digitraffic.fi/api/v2/graphql/graphql"
+    )
+
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    # Execute the GraphQL query on the transport
+    result = client.execute(gql(query))
+
+    return result
